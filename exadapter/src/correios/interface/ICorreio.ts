b9f6b8d38b08879e56b54a935aabef05e428f49a
lineraffaa <alineliber13@gmail.com>
2024-05-24 15:48:11 -0300

    remove

--- exadapter/src/correios/interface/ICorreio.ts
+++
@@ -1,7 +0,0 @@
-import Token from "../../../util/Token";
-
-export default interface ICorreio{
-    authToken(): Token;
-    sendCorreio() : void;
-    receiveCorreio(): void;
-}
--- exadapter/src/index.ts
+++
@@ -1,8 +0,0 @@
-import TransportadoraAdapter from "./Adapter/TransportadoraAdapter";
-import ICorreio from "./correios/interface/ICorreio";
-import Transportadora from "./transporte/Transportadora";
-
-
-const transportadora : ICorreio = new TransportadoraAdapter(new Transportadora());
-transportadora.receiveCorreio();
-transportadora.sendCorreio();
