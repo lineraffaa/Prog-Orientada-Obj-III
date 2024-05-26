da533c8d858f495bd19950dd8870b4bf78f8e3da
lineraffaa <alineliber13@gmail.com>
2024-05-26 20:18:57 -0300

    remove

--- abstraticfactoryexer/src/delivery/interfaces/ideliveryfactory.ts
+++
@@ -1,7 +0,0 @@
-import Drink from "../drink/interfaces/Drink";
-import IFood from "../Ifood/interfaces/Ifood";
-
-export default interface ideliveryfactory{
-    createDeliveryDrink() : Drink;
-    createDeliveryFood(): IFood;
-}
